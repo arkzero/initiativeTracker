4e57e3ee99a626a5c6f28abf34efeb10ad7b094a
Bryan Nissen <bryan@webizly.com>
2015-10-17 14:38:53 -0400

    Initial Functionality

--- app/app.js
+++ app/app.js
@@ -1,12 +1,59 @@
-'use strict';
+(function () {
+  'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  var module = angular.module('myApp', [
+    'ngRoute',
+    'myApp.version'
+  ]).
+  config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/'});
+
+    $routeProvider.when('/', {
+      templateUrl: 'tracker.html',
+      controller: 'TrackerCtrl'
+    });
+  }]);
+
+  module.controller('TrackerCtrl', ['$scope', function ($scope) {
+    $scope.players = [];
+    $scope.enemies = [];
+    $scope.allCombatents = [];
+
+    $scope.playerName = null;
+    $scope.enemyName = null;
+
+    $scope.addPlayer = function (playerName) {
+      $scope.players.push({
+        name: playerName,
+        initiative: null
+      });
+      $scope.playerName = null;
+    };
+
+    $scope.addEnemy = function (enemyName) {
+      $scope.enemies.push({
+        name: enemyName,
+        initiative: null
+      });
+      $scope.enemyName = null;
+    };
+
+    $scope.resetGroup = function (players, allClear) {
+      var group = (players) ? $scope.players : $scope.enemies;
+
+      if (allClear) {
+        $scope.players = [];
+      } else {
+        for (var i = 0; i < $scope.players.length; i++) {
+          group[i].initiative = null;
+        }
+      }
+    };
+
+    $scope.$watchCollection('players + enemies', function () {
+      $scope.allCombatents = $scope.players.concat($scope.enemies);
+    })
+
+  }]);
+})();
